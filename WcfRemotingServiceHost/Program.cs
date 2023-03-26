96dbb2d9b30598e80bea5fa30572fe2e93643057
Kodpencerem <emrullah04@outlook.com>
2023-03-26 18:09:49 +0300

    Add windows form app and update host application.

--- WcfRemotingServiceHost/Program.cs
+++ WcfRemotingServiceHost/Program.cs
@@ -1,9 +1,26 @@
-﻿namespace WcfRemotingServiceHost
+﻿using System;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
+namespace WcfRemotingServiceHost
 {
     public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            GetMessageRemotingService.GetMessageRemotingService remotingService = new GetMessageRemotingService.GetMessageRemotingService();
+
+            TcpChannel channel = new TcpChannel(8081);
+
+            ChannelServices.RegisterChannel(channel);
+
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(GetMessageRemotingService.GetMessageRemotingService), "GetMessage", WellKnownObjectMode.Singleton);
+
+            Console.WriteLine("Remoting service started @ " + DateTime.Now);
+
+            Console.ReadLine();
+
         }
     }
 }
